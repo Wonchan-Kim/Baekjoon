3884b7c08d13799900cc7760a726631fd3336cd4
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2023-06-05 17:34:42 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace::std;
+
+int gcd(int a, int b){
+    return b ? gcd(b, a%b) : a;
+}
+
+int lcm(int a, int b){
+    return (a*b)/gcd(a ,b);
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a,b) << endl;
+    cout << lcm(a,b) << endl;
+    
+}
