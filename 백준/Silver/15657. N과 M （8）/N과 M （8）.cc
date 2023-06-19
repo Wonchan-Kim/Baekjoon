f9ac350af6798b3a29dc253e1d63c3e71ebf3285
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2023-06-19 21:09:43 +0900

    [Silver III] Title: N과 M (8), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/*
+TODO: 중복되는 수열 x, 오름차순
+CHECKLIST:  1. 백트래킹 조건을 만족하는가? 2. 이미 지나간 수에 대한 처리가 되었는가? 3.출력을 마친뒤 종료 하였는가?
+
+*/
+using namespace::std;
+int M, N;
+vector<int> store;
+//bool check[10];
+int arr[9];
+
+void backtracking(int len){
+    if(len == N){
+        for(int i = 0; i < N; i++){
+            cout << store[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 0; i < M; i++){
+        if(store.empty()){
+            store.push_back(arr[i]);
+            backtracking(len+1);
+            store.pop_back();
+        }
+        else if(store.back() <= arr[i]){
+                store.push_back(arr[i]);
+                backtracking(len+1);
+                store.pop_back();
+    
+        }
+    }
+}
+
+int main(){
+    cin >> M >> N; //NOTE: M is the range, N is the length
+    for(int i = 0; i < M; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr + M);
+    backtracking(0);
+}
