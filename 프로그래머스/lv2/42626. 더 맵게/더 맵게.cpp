85d9537a628696ebcde4c391f99de5e445938789
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2023-07-18 17:07:37 +0900

    [level 2] Title: 더 맵게, Time: 234.67 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int> pq;
+    int q_top;
+    int mix;
+    vector<int>::iterator it;
+    
+    for(it = scoville.begin(); it != scoville.end(); it++){
+        *it *= -1;
+        pq.push(*it);
+    }
+    while(pq.size() > 1 && pq.top() > -K){
+        q_top = pq.top();
+        pq.pop();
+        mix = pq.top();
+        pq.pop();
+        q_top += mix * 2;
+        pq.push(q_top);
+        answer++;
+    }
+    
+    return pq.top() <= -K ? answer : -1;
+}
+/*
+the values will be sorted into descending order.
+[1, 2, 3, 9, 10, 12]	7	2
+
+in the following example, 
+1
+2
+3
+9
+10
+12
+
+this is how queue will be sorted.
+
+always check whether the top element of the queue is smaller or larger than the K.
+if smaller, pop two elements, and mix and put it in the queue again. 
+*/
