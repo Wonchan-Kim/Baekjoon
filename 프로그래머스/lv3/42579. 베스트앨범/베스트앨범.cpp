66319b1b3324fe1823937f739f940e8776f9c758
Wonchan-Kim <81521208+Wonchan-Kim@users.noreply.github.com>
2023-07-28 15:57:15 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
@@ -2,64 +2,54 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 class Song {
 public:
     int identifier;
-    string genre;
-    int play;
+    int plays;
 
-    Song(int identifier, string genre, int play) : identifier(identifier), genre(genre), play(play) {}
+    Song(int identifier, int plays) : identifier(identifier), plays(plays) {}
 };
 
-bool sortByPlayThenId(const Song& a, const Song& b) {
-    if (a.play != b.play)
-        return a.play > b.play;
-    return a.identifier < b.identifier;
+bool sortByPlaysDesc(const Song& a, const Song& b) {
+    return a.plays > b.plays;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     vector<Song> songs;
-
-    // Step 1: Parse input arrays and populate custom data structure
-    for (int i = 0; i < genres.size(); i++) {
-        songs.emplace_back(i, genres[i], plays[i]);
+    for (size_t i = 0; i < genres.size(); ++i) {
+        songs.emplace_back(Song(i, plays[i]));
     }
 
-    // Step 2: Group songs by genre in a map
-    map<string, vector<Song>> songMap;
-    for (const auto& song : songs) {
-        songMap[song.genre].push_back(song);
+    map<string, vector<Song>> myMap;
+    for (size_t i = 0; i < genres.size(); ++i) {
+        myMap[genres[i]].emplace_back(songs[i]);
     }
-
-    // Step 3: Sort songs within each genre based on play count and identifier
-    for (auto& pair : songMap) {
-        sort(pair.second.begin(), pair.second.end(), sortByPlayThenId);
-    }
-
-    // Step 4: Sort genres based on total play count of all songs in each genre
-    vector<pair<string, int>> genrePlayCounts;
-    for (const auto& pair : songMap) {
+//맵은 정렬 함수 사용 불가,.,,
+    
+    vector<pair<int, string>> genrePlayCount;
+    for (const auto& pair : myMap) {
         int totalPlayCount = 0;
-        for (const auto& song : pair.second) {
-            totalPlayCount += song.play;
+        const vector<Song>& songs = pair.second;
+        for (const Song& song : songs) {
+            totalPlayCount += song.plays;
         }
-        genrePlayCounts.emplace_back(pair.first, totalPlayCount);
+        genrePlayCount.emplace_back(totalPlayCount, pair.first);
     }
-    sort(genrePlayCounts.begin(), genrePlayCounts.end(), [](const auto& a, const auto& b) {
-        return a.second > b.second;
-    });
+    //1번 조건
 
-    // Step 5: Select the top two songs from each genre and add their identifiers to the answer vector
-    for (const auto& pair : genrePlayCounts) {
-        const string& genre = pair.first;
-        const vector<Song>& genreSongs = songMap[genre];
-        int count = min(2, static_cast<int>(genreSongs.size()));
-        for (int i = 0; i < count; i++) {
-            answer.push_back(genreSongs[i].identifier);
+    sort(genrePlayCount.rbegin(), genrePlayCount.rend());
+
+    for (const auto& pair : genrePlayCount) {
+        const string& genre = pair.second;
+        vector<Song>& songs = myMap[genre];
+        sort(songs.begin(), songs.end(), sortByPlaysDesc);
+        for (size_t i = 0; i < songs.size() && i < 2; ++i) {
+            answer.emplace_back(songs[i].identifier);
         }
     }
 
