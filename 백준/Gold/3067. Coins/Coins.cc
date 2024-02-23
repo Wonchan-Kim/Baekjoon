d00c7b58d01334873ba00f490daba5991eea3569
Wonchan-Kim <wonchankim000827@gmail.com>
2024-02-23 15:31:48 -0800

    [Gold V] Title: Coins, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/3067. Coins/Coins.cc
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <string.h>
 #include <vector>
-#include <string.h> // For memset
+
 
 using namespace std;
 
 int dp[10001];
 
 int bottomTop(vector<int>& value, int n, int m){
-    memset(dp, 0, sizeof(dp)); // Reset dp array for each call
-    dp[0] = 1; // Base case: there's one way to make 0, which is to use no coins
-    for(int i = 0; i < n; i++){ // Corrected indexing to start from 0
+    dp[0] = 1;
+    for(int i = 0; i < n; i++){
         for(int j = value[i]; j <= m; j++){
             dp[j] += dp[j-value[i]];
         }
@@ -23,13 +23,13 @@ int main() {
     while(t--){
         int n; // type of coins
         cin >> n;
-        vector<int> value(n); // Pre-size the vector to n
+        vector<int> value(n);
         for(int i = 0; i < n; i++){
             cin >> value[i];
         }
-        int m; // Target value
+        int m; //target value
         cin >> m;
         cout << bottomTop(value, n, m) << endl;
-        // No need to memset here since it's done at the beginning of bottomTop
+        memset(dp,0,sizeof(dp));
     }
 }
