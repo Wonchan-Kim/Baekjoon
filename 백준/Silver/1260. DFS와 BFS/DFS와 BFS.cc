f5c48c3e274f7995a7a5d0eb29bca92cade1c58b
Wonchan-Kim <wonchankim000827@gmail.com>
2024-03-03 14:53:34 -0800

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -2,82 +2,65 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-using namespace::std;
+#include <cstring>
+#include <memory.h>
+using namespace std;
+vector<int> result;
+void dfs(const vector<vector<int>>& graph, vector<bool>& visited, const int& search){
+    visited[search] = true;
+    result.push_back(search);
 
-int v, e; // 정점 , 간선
-vector<vector<int>> graph;
-vector<bool> isVisited;
-
-void input(int v, int e)
-{
-    
-    graph.assign(v + 1, vector<int> (0,0)); //인덱스 0에 0을 넣고 정점 개수 만큼 복사
-    isVisited.assign(v + 1, false);
-    
-    for(int i = 0; i < e; i++)
-    {
-        int s, e;
-        cin >> s >> e;
-        graph[s].emplace_back(e);
-        graph[e].emplace_back(s);
+    for(int i = 0; i < graph[search].size(); i++){
+        if(!visited[graph[search][i]]){
+            dfs(graph, visited, graph[search][i]);
+        }
     }
-    for(int i = 0; i < v+ 1; i++)
-        sort(graph[i].begin(), graph[i].end());
 }
 
-void BFS(int cur)
-{
-    queue <int> q;
-    q.push(cur);
-    isVisited[cur] = true;
-    
-    
-    while(!q.empty())
-    {
-        int x = q.front();
+void bfs(const vector<vector<int>>& graph, vector<bool>& visited, const int& search){
+    queue<int> q;
+    q.push(search);
+    visited[search] = true;
+    while(!q.empty()){
+        int temp = q.front();
         q.pop();
-        cout << x << ' ';
-        for(int i = 0; i < graph[x].size(); i++)
-        {
-            int y = graph[x][i];
-            if(!isVisited[y]){
-                q.push(y);
-                isVisited[y] = true;
-                
+        result.push_back(temp);
+
+        for(int i = 0; i < graph[temp].size(); i++){
+            if(!visited[graph[temp][i]]){
+                q.push(graph[temp][i]);
+                visited[graph[temp][i]] = true;
             }
         }
     }
-    
 }
 
-void DFS(int cur)
-{
-    isVisited[cur] = true;
-    cout<< cur << ' ';
-    
-    for(int i = 0; i < graph[cur].size(); i++)
-    {
-        int next = graph[cur][i];
-        if(!isVisited[next])
-            DFS(next);
+int main() {
+
+    int n, m, v;
+    cin >> n >> m >> v; //n is edge, m is vertex, v is to search
+    vector<bool> visited(n+1);
+    vector<vector<int>> graph(n+1);
+    while(m--) {
+        int start, end;
+        cin >> start >> end;
+        graph[start].push_back(end);
+        graph[end].push_back(start);
+        //non-directed graph
     }
-}
-
-int main()
-{
-    int v , e;
-    cin >> v >> e;
-    int i;
-    cin >> i;
-    input(v,e);
-    
-    DFS(i);
-    cout << '\n';
-  
-    
-    for(int itr = 0; itr <= v; itr++)
-        isVisited[itr] = false;
-    
-      BFS(i);
-    
+    for(int i = 1; i <= n; i++){
+        sort(graph[i].begin(), graph[i].end());
+    }
+    dfs(graph, visited, v);
+    std::fill(visited.begin(), visited.end(), false); // Reset visited for DFS or any further processing
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i] << ' ';
+    }
+    cout << endl;
+    result.clear();
+    bfs(graph, visited, v);
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i] << ' ';
+    }
+    cout << endl;
 }
